d24ee466cd7511707ff0918d26dbb456997e9f4e
immaGodgivenmasterpiece <ryan.seungmin@gmail.com>
2025-12-29 15:26:36 +0900

    fix: Update NavigationMenu component to disable viewport behavior for better layout control

--- components/header.tsx
+++ components/header.tsx
@@ -88,7 +88,7 @@ export function Header() {
         </Link>
 
         {/* Desktop Navigation */}
-        <NavigationMenu className="hidden lg:flex">
+        <NavigationMenu className="hidden lg:flex" viewport={false}>
           <NavigationMenuList>
             {menuItems.map((item) => (
               <NavigationMenuItem key={item.title}>
